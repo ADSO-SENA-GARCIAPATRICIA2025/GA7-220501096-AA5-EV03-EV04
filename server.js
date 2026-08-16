b4204abea2103bd6b4145a88ac8b98f5781398fe
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2026-08-16 22:52:36 +0200

    PASO 8 — Conectar las rutas en server.js

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import express from "express"; 
-import db from "./config/db.js";
+import db from "./config/db.js"; 
+import productoRoutes from './routes/productoRoutes.js';
+
+
 const app= express();
 const PORT = 3000; 
+
+
 app.use(express.json());
+app.use('/productos', productoRoutes);
+
+
+
+
 app.get("/", (req, res)=> {
     res.json({
-    mensaje: "api footsy funciona OK"
+    mensaje: "api footsy funciona OK"});
 });
-});
+
+app.use('/productos', productoRoutes);
+
+
+
 app.listen(PORT, () =>{
     console.log(`servidor funcionante en localhost: ${PORT}`)
 });
