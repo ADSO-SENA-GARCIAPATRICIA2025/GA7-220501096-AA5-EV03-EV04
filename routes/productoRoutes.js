2c25f38e555d356a8cd42c1c5304bd9d024700c4
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2026-08-16 22:24:59 +0200

    PASO 7 — Crear la ruta de Producto

---
+++ routes/productoRoutes.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import { listarProductos } from '../controllers/productoController.js';
+const router = express.Router();
+router.get('/', listarProductos);
+export default router;
